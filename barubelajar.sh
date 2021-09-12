c22669e6b94b1aa69e82c6d8ddb09e676375c50a
BondnetwordS1 <88929961+BondnetwordS1@users.noreply.github.com>
2021-09-12 11:41:42 +0700

    Add files via upload

---
+++ barubelajar.sh
@@ -0,0 +1,26 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m' 
+cyan='\033[36;1m' 
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m' 
+sleep 1
+echo
+toilet -f big -F gay Hack wifi
+echo $purple"selamat datang di hack wifi"
+echo $purple"silakan anda lihat peraturan di bawah"
+echo
+echo $blue"praturan"
+echo $red"script ini hanya berfungsi di daerah ter tentu"
+echo $red"seperti jakarta"
+echo $red"ok jika sudah paham silakan lihat di bawah"
+echo
+echo
+echo $cyan"pilih password dan nama"
+echo
+echo $cyan"1.) usr = DAP-SMP"
+echo $cyan"    pas = semuabisasekolah"
+echo
+echo $cyan"2.) coming soon"
